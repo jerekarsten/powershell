65401fb40bb9e8ec39bacfc20b77bb17321fe7fb
Jere Karsten <jere.karsten@gmail.com>
2017-11-01 21:59:00 +0200

    Uploaded Dwarf Fortress gamelog tailing script with color coding and word filtering

---
+++ tail_gamelog.ps1
@@ -0,0 +1,42 @@
+﻿# locate gamelog.txt, in the base dir of Dwarf Fortress
+$Logfile = "H:\Games\DF\Dwarf Fortress 0.43.05\gamelog.txt"
+
+# censor words
+$global:disable ="retches"
+    
+# assign colors, separate words or sentences with |
+$global:blue        = "mandate has"
+$global:cyan        = "masterpiece"
+#$global:darkblue   = ""
+#$global:darkgreen  = ""
+#$global:darkgrey   = ""
+#$global:darkmagenta = ""
+#$global:darkred    = ""
+$global:darkyellow  = "migrant"
+$global:green       = "birth"
+$global:grey        = "has become"
+$global:magenta     = "been struck"
+$global:red         = "cancel|withdraw"
+$global:yellow      = "have struck|mandated"
+
+# things happen
+function Get-LogColor {
+    Param([Parameter(Position=0)]
+    [String]$LogEntry)
+
+    process {
+        
+        if     ($LogEntry|where{$_ -match $green})  {Return "Green"}
+        elseif ($LogEntry|where{$_ -match $yellow}) {Return "Yellow"}
+        elseif ($LogEntry|where{$_ -match $darkYellow}) {Return "DarkYellow"}
+        elseif ($LogEntry|where{$_ -match $red})    {Return "Red"}
+        elseif ($LogEntry|where{$_ -match $blue})    {Return "Blue"}
+        elseif ($LogEntry|where{$_ -match $cyan})    {Return "Cyan"}
+        elseif ($LogEntry|where{$_ -match $magenta})    {Return "Magenta"}
+        elseif ($LogEntry|where{$_ -match $grey})    {Return "Grey"}
+        else {Return "White"}
+
+
+    }
+}
+Get-Content -wait $logFile -tail 400 |where {$_ -notmatch $disable} | ForEach { Write-Host -ForegroundColor (Get-LogColor $_) $_}
