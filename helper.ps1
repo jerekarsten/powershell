efbb7c75ea44e005b418e3de5aa818e8e351b6cb
Jere Karsten <jere.karsten@helsinki.fi>
2018-10-27 14:57:49 +0300

    upload stuff

---
+++ helper.ps1
@@ -0,0 +1,6 @@
+﻿$gameLog = "H:\Games\DF\Dwarf Fortress 0.43.05\gamelog.txt"
+Get-Content -Path $gameLog -Wait -tail 200 | where { $_ -match "you have struck" } |sort -Unique
+
+# orelistaus
+$orelist = (Get-Content -Path $gameLog | where { $_ -match "you have struck" } |sort -Unique).Replace("You have struck","").Replace("!","") |Out-GridView
+
--- tail_gamelog.1.ps1
+++ tail_gamelog.1.ps1
@@ -2,10 +2,7 @@
 ## by Urist McKarsten 11/2017
 
 # locate gamelog.txt, in the base dir of Dwarf Fortress
-Param(
-    $DFFolder ="H:\Games\DF\Dwarf Fortress 0.43.05" # <- dwarf fortess installation folder here, without trailing slash
-    )
-$LogFile = "$DFFolder\gamelog.txt"
+$Logfile = "H:\Games\DF\Dwarf Fortress 0.43.05\gamelog.txt" # <- change your gamelog file here
 
 # censor words
 $global:disable ="retches"
@@ -20,15 +17,15 @@ $global:cyan        = "masterpiece"
 $global:darkblue    = "attack"
 $global:darkcyan    = "been completed"
 $global:darkgreen   = "been struck|stuck in"
-#$global:darkgray    = ""
+$global:darkgray    = ""
 $global:darkmagenta = "dead"
-#$global:darkred     = "vomit"
+$global:darkred     = "vomit"
 $global:darkyellow  = "migrant"
 $global:green       = "birth|calendar"
 $global:gray        = "has become"
 $global:magenta     = "missing"
 $global:red         = "cancel|withdraw"
-$global:yellow      = "have struck|mandated"
+$global:yellow      = "have struck|spring|summer|autumn|winter"
 
 
 
@@ -39,23 +36,22 @@ function Get-LogColor {
 
     process {
         
-        
-        if ($LogEntry|where{$_ -match $blue})       {Return "Blue"}
-        elseif ($LogEntry|where{$_ -match $cyan})       {Return "Cyan"}
-        elseif ($LogEntry|where{$_ -match $darkblue})   {Return "DarkBlue"}
-        elseif ($LogEntry|where{$_ -match $darkgreen})  {Return "DarkGreen"}
-        #elseif ($LogEntry|where{$_ -match $darkgray})   {Return "DarkGray"}
-        elseif ($LogEntry|where{$_ -match $darkcyan})   {Return "DarkCyan"}
-        elseif ($LogEntry|where{$_ -match $darkmagenta}){Return "DarkMagenta"}
-        #elseif ($LogEntry|where{$_ -match $darkred})    {Return "DarkRed"}
-        elseif ($LogEntry|where{$_ -match $darkYellow}) {Return "DarkYellow"}
-        elseif ($LogEntry|where{$_ -match $green})      {Return "Green"}
-        elseif ($LogEntry|where{$_ -match $gray})       {Return "Gray"}
-        elseif ($LogEntry|where{$_ -match $magenta})    {Return "Magenta"}
-        elseif ($LogEntry|where{$_ -match $red})        {Return "Red"}
-        elseif ($LogEntry|where{$_ -match $yellow})     {Return "Yellow"}
-
-        else {Return "White"}
+        switch -regex ($LogEntry){
+            $blue       {"Blue"}
+            $cyan       {"Cyan"}
+            $gray       {"Gray"}
+            $magenta    {"Magenta"}
+            $red        {"Red"}
+            $yellow     {"Yellow"}
+            $darkblue   {"DarkBlue"}
+            $darkgreen  {"DarkGreen"}
+            #$darkgray   {"DarkGray"}
+            $darkcyan   {"DarkCyan"}
+            $darkmagenta{"DarkMagenta"}
+            $darkred    {"DarkRed"}
+            $darkYellow {"DarkYellow"}
+            Default     {"White"}
+            }
 
     }
 }
@@ -75,12 +71,11 @@ function notify {
     Param([Parameter(Position=0)]
     [String]$LogEntry)
     
-    if ($LogEntry|where{$_ -match $notify -and $enableNotify -eq $true}){
-        # comment to disable beep
+    if ($LogEntry -match $notify -and $enableNotify -eq $true){
+        # comment to only disable beep
         # [console]::beep(1000,1000)
         # play a sequence of beeps
         # Invoke-Beep
-
         Return "!!___" + $_ + "___!!"}
     
     else{Return $_}
